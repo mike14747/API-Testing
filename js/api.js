bfc2cd628e330c9ffbe1a3cfa3a3d5302df1b213
mike4747 <mike4747@oh.rr.com>
2019-03-14 21:48:19 -0400

    testing openweather api

--- js/api.js
+++ js/api.js
@@ -2,27 +2,62 @@
 
 var regExp = /^(\d{5})?$/;
 var zipCode = "";
-var apiKey = "4af80d5a852a405d9baad6ce23a015b0";
-var queryURL = "";
 
 function zipSearch(zip) {
-    queryURL = "https://zipcodedownload.com:5430/Filter?format=json&citytype=d&cityname=&postalcode=" + zip + "&country=us5&key=" + apiKey;
+    // zipcodedownload.com api key
+    var apiKey = "4af80d5a852a405d9baad6ce23a015b0";
+    var queryURL = "https://zipcodedownload.com:5430/Filter?format=json&citytype=d&cityname=&postalcode=" + zip + "&country=us5&key=" + apiKey;
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
+        if (response.length > 0) {
+            console.log(zip);
+            $("#zip_info").show();
+            $("#zip_code").text(zip);
+            $("#zip_city").text(response[0].city_name);
+            $("#zip_state").text(response[0].province);
+            $("#zip_lat").text(response[0].lat);
+            $("#zip_lon").text(response[0].lon);
+            $("#zip_ac").text(response[0].area_code);
+            $("#zip_tz").text(response[0].time_zone);
+        }
     });
+    return;
+}
+
+function weather(zip) {
+    // https://api.weather.gov/points/41.75,-81.2833
+    // then, use the data from: response.properties.forecast to make the following ajax call:
+    // https://api.weather.gov/gridpoints/CLE/95,77/forecast
+    // openweathermap.org api key
+
+    var apiKey = "4245352a3814173935fcebaa7e744e45";
+    // api.openweathermap.org/data/2.5/forecast?id=524901&APPID=1111111111
+    var queryURL = "api.openweathermap.org/data/2.5/forecast?zip=" + zip + "&units=imperial&cnt=1&APPID=" + apiKey;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        if (response.length > 0) {
+            console.log(response);
+            
+        }
+    });
+    return;
 }
 
 $("#submit_zip").on("click", function () {
     event.preventDefault()
-    zipCode = $("#zip_code").val().trim();
+    zipCode = $("#zip_code_search").val().trim();
     if (zipCode == "" || !zipCode.match(regExp)) {
         console.log("Validation failed");
     } else {
         // zip code was submitted
-        zipSearch(zipCode);
+        $("#results_row").removeClass("d-none");
+        // zipSearch(zipCode);
+        weather(zipCode);
     }
 });
