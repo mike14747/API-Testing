37447dcec379ee5d15c98dd6ed5bce86a5c897e6
mike4747 <mike4747@oh.rr.com>
2019-03-16 21:11:58 -0400

    testing something in auth redirect

--- js/login.js
+++ js/login.js
@@ -14,7 +14,7 @@ var uiConfig = {
     },
     // will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: 'index.html',
+    // signInSuccessUrl: 'index.html',
     signInOptions: [
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         firebase.auth.GithubAuthProvider.PROVIDER_ID,
