736751ec2a8239ae0bd9f479f311df735c001036
mike4747 <mike4747@oh.rr.com>
2019-03-16 21:57:31 -0400

    fixed login redirect?

--- js/login.js
+++ js/login.js
@@ -21,16 +21,9 @@ var uiConfig = {
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
       firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      firebase.auth.PhoneAuthProvider.PROVIDER_ID
-    ],
-    // Terms of service url.
-    tosUrl: '<your-tos-url>',
-    // Privacy policy url.
-    privacyPolicyUrl: '<your-privacy-policy-url>'
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ]
   };
 
 // callbacks: { 'signInSuccess': function(currentUser, credential, redirectUrl) { return false; }
@@ -51,6 +44,7 @@ firebase.auth().onAuthStateChanged(function (user) {
         // user is signed in
         console.log("Yes");
         $("#log_in").hide();
+        $("#loginModal").hide();
         $("#log_out").show();
     } else {
         // user is not signed in
